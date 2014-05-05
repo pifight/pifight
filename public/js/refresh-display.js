4b7f53cfecda3151a96056ac70eab6cf143d93eb
Michael Brodhead <mkb@orthogonal.org>
2014-05-04 21:57:44 -0700

    jquery

---
+++ public/js/refresh-display.js
@@ -0,0 +1,22 @@
+      var s = Snap("#svg");
+      s.attr({width: 600, height: 300});
+      window.onload = function () {
+        Snap.load("bots/bot1.svg", function (f) {
+            f.select("svg").attr({fill: "#bada55", width: "1.5em", height: "1.5em", x: "10", y: "10"});
+            // g = f.select("g");
+            s.append(f);
+          });
+      Snap.load("bots/bot2.svg", function (f) {
+          f.select("svg").attr({fill: "#aaaaaa", width: "1.5em", height: "1.5em", x: "50", y: "20"});
+          // g = f.select("g");
+          s.append(f);
+        });
+
+      Snap.load("bots/bot3.svg", function (f) {
+          f.select("svg").attr({fill: "#ad413a", width: "1.5em", height: "1.5em", x: "100", y: "20"});
+          // g = f.select("g");
+          s.append(f);
+        });
+
+      }
+
