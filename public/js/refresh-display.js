11856305050d70efe429bd2964182ca9966c0f57
Michael Brodhead <mkb@orthogonal.org>
2014-05-04 23:25:52 -0700

    updating health too. guard code for mysterious error. still need real fix.

--- public/js/refresh-display.js
+++ public/js/refresh-display.js
@@ -8,7 +8,12 @@ $( document ).ready(function() {
 
 function updateBots(botInfo) {
   for (bot in botInfo) {
-    s.select("#" + bot).attr({x: botInfo[bot].x, y: botInfo[bot].y})
+    if (bot) {
+      console.log("#" + bot);
+      console.log(s.select("#" + bot))
+      s.select("#" + bot).attr({x: botInfo[bot].x, y: botInfo[bot].y});
+      $("#" + bot + "-health").text(botInfo[bot].health);
+    }
   }
 }
 
@@ -16,20 +21,17 @@ function botSetup() {
   Snap.load("bots/bot1.svg", function (f) {
       f.select("svg").attr({fill: "#bada55", width: "1.5em",
         height: "1.5em", x: "10", y: "10", id: "bot1"});
-      // g = f.select("g");
       s.append(f);
     });
   Snap.load("bots/bot2.svg", function (f) {
       f.select("svg").attr({fill: "#aaaaaa", width: "1.5em",
         height: "1.5em", x: "50", y: "20", id: "bot2"});
-      // g = f.select("g");
       s.append(f);
     });
 
   Snap.load("bots/bot3.svg", function (f) {
       f.select("svg").attr({fill: "#ad413a", width: "1.5em",
         height: "1.5em", x: "100", y: "20", id: "bot3"});
-      // g = f.select("g");
       s.append(f);
     });
 }
