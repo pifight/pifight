3875b4a802e64223b7e92f2992c44764f90f02f2
Michael Brodhead <mkb@orthogonal.org>
2014-06-19 23:08:50 -0700

    add trivial web responder

--- lib/pifight/router.ex
+++ lib/pifight/router.ex
@@ -2,6 +2,10 @@ defmodule Pifight.Router do
   use Plug.Router
   import Plug.Conn
 
+  get "/bots/:id" do
+    conn |> resp(200, inspect(id))
+  end
+
   match _ do
     send_resp(conn, 200, "Hello, world!")
   end
--- mix.exs
+++ mix.exs
@@ -2,24 +2,24 @@ defmodule Pifight.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :pifight,
-     version: "0.0.1",
-     elixir: "~> 0.14.0",
-     deps: deps]
+    [ app: :pifight,
+    version: "0.0.1",
+    elixir: "~> 0.14.0",
+    deps: deps ]
   end
 
   # Configuration for the OTP application
   # Type `mix help compile.app` for more information
   def application do
     [ applications: [:cowboy, :plug],
-      mod: { Pifight, []}
-    ]
+    mod: { Pifight, []} ]
   end
 
   #   {:mydep, "~> 0.3.0"}
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
   defp deps do
     [{ :plug, github: "elixir-lang/plug" },
-      { :cowboy, github: "extend/cowboy" }]
+    { :cowboy, github: "extend/cowboy" },
+    { :httpotion, github: "myfreeweb/httpotion"} ]
   end
 end
